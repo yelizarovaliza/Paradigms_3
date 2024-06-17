6f9f75ebb728eca7f517a66cd844d086207b434f
yelizarovaliza <yyelizarova@kse.org.ua>
2024-06-17 22:43:55 +0300

    Caesar Cioher added

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
+class CaesarCipher {
+public:
+    static char* encrypt(char* rawText, int key) {
+        int len = strlen(rawText);
+        char* encryptedText = new char[len + 1];
+
+        for (int i = 0; i < len; ++i) {
+            char c = rawText[i];
+            if (isalpha(c)) {
+                char base = isupper(c) ? 'A' : 'a';
+                encryptedText[i] = (c - base + key) % 26 + base;
+            }
+            else {
+                encryptedText[i] = c;
+            }
+        }
+        encryptedText[len] = '\0';
+        return encryptedText;
+    }
+
+    static char* decrypt(char* encryptedText, int key) {
+        return encrypt(encryptedText, 26 - key);
+    }
+};
+
 int main() {
-    cout << "Hello world" << endl;
+    char rawText[] = "Roses are red, violets are blue";
+    int key = 1;
+
+    char* encrypted = CaesarCipher::encrypt(rawText, key);
+    char* decrypted = CaesarCipher::decrypt(encrypted, key);
+
+    cout << "Original Text: " << rawText << endl;
+    std::cout << "Encrypted Text: " << encrypted << endl;
+    cout << "Decrypted Text: " << decrypted << endl;
+
+    delete[] encrypted;
+    delete[] decrypted;
+
     return 0;
 }
